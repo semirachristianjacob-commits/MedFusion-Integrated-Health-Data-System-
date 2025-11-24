eb6ae95ec10019516d8c4954f21c9af17d166b2c
rencymabangis <melanayrency@gmail.com>
2025-11-24 16:53:43 +0800

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,24 +1,24 @@
 public abstract class Patient {
-private String id;
-private String name;
-private int age;
-private String diagnosis;
-public Patient(String id, String name, int age, String diagnosis) {
+    private String id;
+    private String name;
+    private int age;
+    private String diagnosis;
+
+    public Patient(String id, String name, int age, String diagnosis) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.diagnosis = diagnosis;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getDiagnosis() { return diagnosis; }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public abstract void showInfo();
 }
-this.id = id;
-this.name = name;
-this.age = age;
-this.diagnosis diagnosis;
-public String getId() { return id; }
-public String getName() { return name; }
-public int getAge() { return age; }
-public String getDiagnosis() { return
-diagnosis; }
-public void setDiagnosis (String diagnosis)
-{ this.diagnosis diagnosis; }
-public abstract void showInfo();
-}
-
-
-
-
